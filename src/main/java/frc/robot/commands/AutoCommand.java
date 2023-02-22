2c72e2d7148a6cea0dc4df4f294fcda15bed35b3
Gdenzien <4hcocoa@gmail.com>
2023-02-21 19:25:12 -0600

    Auto drive only

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -1,18 +1,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoCommand extends SequentialCommandGroup {
 
-    public AutoCommand(){
+    public AutoCommand(DriveSubsystem Drive){
 
         addCommands(
+
+            new DriveToPosition(Drive, Constants.INCHES_TO_TICKS_AMT(181.435))
             // Add Commands here:
             // Also add parallel commands using the
             //
             // addCommands(
             //      new command1(argsN, subsystem),
-            //      parallel(
+            //      Commands.parallel(
             //          new command2(argsN, subsystem),
             //          new command3(argsN, subsystem)
             //      )    
