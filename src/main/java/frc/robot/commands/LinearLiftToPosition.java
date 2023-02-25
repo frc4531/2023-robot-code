8fedc485eb4799b2447abec581c8e9c20407e67e
Sam T <corgidadog@gmail.com>
2023-02-25 15:32:55 -0600

    implement togglable positions using limit switches

--- src/main/java/frc/robot/commands/LinearLiftToPosition.java
+++ src/main/java/frc/robot/commands/LinearLiftToPosition.java
@@ -35,21 +35,42 @@ public class LinearLiftToPosition extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        
+        if (IsCentering) { // If centering the lift on the current stage previously set
+            if (m_linearLiftSubsystem.GetPosition() < 0) { // If the lift is lower than it is supposed to be, move up
+                m_linearLiftSubsystem.SetLinearLiftMotors(liftSpeed);
+            } else if (m_linearLiftSubsystem.GetPosition() > 0) { // If the lift is higher that it is supposed to be, move down
+                m_linearLiftSubsystem.SetLinearLiftMotors(-liftSpeed);
+            }
+        } else { // If moving the lift from one stage to another, different stage
+            if (targetPosition > m_linearLiftSubsystem.GetStage()) { // Going up!
+                m_linearLiftSubsystem.SetLinearLiftMotors(liftSpeed);
+            } else if(targetPosition < m_linearLiftSubsystem.GetStage()) { // Going down!
+                m_linearLiftSubsystem.SetLinearLiftMotors(-liftSpeed);
+            }   
+        }
     }
-
 
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
         m_linearLiftSubsystem.SetLinearLiftMotors(0);
+        
+        if (!interrupted) {
+            m_linearLiftSubsystem.ResetEncoder();
+        }
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+       if (IsCentering) {
+        // If centering the lift on current stage, finish the command when the lift is within margin of error of the zero point
+        return ((m_linearLiftSubsystem.GetPosition() >= -movementThreshold) && (m_linearLiftSubsystem.GetPosition() <= movementThreshold));
+       } else {
+        // End the command when the subsystem says we're at the target position (limit switch is triggered)
+         return m_linearLiftSubsystem.GetStage() == targetPosition;
+       }
     }
 
     @Override
