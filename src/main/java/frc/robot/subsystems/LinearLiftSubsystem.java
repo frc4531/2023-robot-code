8e495d12cc427c3255a5b6a76a9f7d974018bc07
Sam T <corgidadog@gmail.com>
2023-02-25 13:50:52 -0600

    Implement magentic limit switchs for lift

--- src/main/java/frc/robot/subsystems/LinearLiftSubsystem.java
+++ src/main/java/frc/robot/subsystems/LinearLiftSubsystem.java
@@ -1,18 +1,25 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class LinearLiftSubsystem extends SubsystemBase {
 
     private WPI_TalonFX linearPulleyMotor;
+    private DigitalInput GroundSwitch;
+    private DigitalInput LowSwitch;
+    private DigitalInput MediumSwitch;
+    private DigitalInput HighSwitch;
 
     public LinearLiftSubsystem() {
         linearPulleyMotor = new WPI_TalonFX(9);
- 
- 
+        GroundSwitch = new DigitalInput(4);
+        LowSwitch = new DigitalInput(5);
+        MediumSwitch = new DigitalInput(6);
+        HighSwitch = new DigitalInput(7);
 
-
+ 
     }
 
     @Override
@@ -29,6 +36,22 @@ public class LinearLiftSubsystem extends SubsystemBase {
 
     public void SetLinearLiftMotors(double Speed) {
         linearPulleyMotor.set(Speed);
+    }
+
+    public boolean ReadGroundSwitch() {
+        return GroundSwitch.get();
+    }
+
+    public boolean ReadLowSwitch() {
+        return LowSwitch.get();
+    }
+
+    public boolean ReadMediumSwitch() {
+        return MediumSwitch.get();
+    }
+
+    public boolean ReadHighSwitch() {
+        return HighSwitch.get();
     }
 
     public int GetPosition() {
